5b2dce9222f7dad65156de3059c94acc962684f4
syw(laptop) <syw9846@naver.com>
2023-10-28 16:16:09 +0900

    Update grass.py

--- game/grass.py
+++ game/grass.py
@@ -1,14 +1,16 @@
 from pico2d import load_image
 
+
 class Grass1:
     def __init__(self):
         self.image = load_image('beachbkg.png')
 
     def draw(self):
-        self.image.draw(400,300,800,600)
+        self.image.draw(400, 300, 800, 600)
 
     def update(self):
         pass
+
 
 class Grass2:
     def __init__(self):
